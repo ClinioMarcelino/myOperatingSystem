f652ec714078291da58196810d933c8380dae55d
Clinio Marcelino <cliniof@gmail.com>
2024-03-29 02:07:57 -0500

    First bootable image

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,19 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> !{
+    let vga_bufferr = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe{
+            *vga_bufferr.offset(i as isize * 2) = byte;
+            *vga_bufferr.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop{}
 }
 
@@ -12,5 +23,7 @@ pub extern "C" fn _start() -> !{
 fn panic(_info: &PanicInfo) -> ! {
     loop{}
 }
+
+
 
 
