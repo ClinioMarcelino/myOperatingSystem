3998176f93fcbb8adf149d922758ff910cd0a339
Clinio Marcelino <cliniof@gmail.com>
2024-04-01 15:56:33 -0500

    match keys

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,3 +1,4 @@
+use pc_keyboard::DecodedKey;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use crate:: {println, print};
 use lazy_static::lazy_static;
@@ -77,12 +78,29 @@ extern "x86-interrupt" fn timer_interrupt_handler(
 
 extern "x86-interrupt" fn keyboard_interrupt_handler(
     _stack_frame: InterruptStackFrame){
+        use pc_keyboard::{layouts, DecodeState ,HandleControl, Keyboard, ScancodeSet1};
+        use spin::Mutex;
         use x86_64::instructions::port::Port;
 
-        let mut port = Port::new(0x60);
-        let scancode: u8 = unsafe{port.read()};
+        lazy_static! {
+            static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> = 
+                Mutex::new(Keyboard::new(layouts::Us104Key, ScancodeSet1,HandleControl::Ignore)
+            );
+        }
 
-        print!("{}", scancode);
+        let mut keyboard = KEYBOARD.lock();
+        let mut port = Port::new(0x60);
+
+        let scancode: u8 = unsafe{ port.read() };
+        if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+            if let Some(key) = keyboard.process_keyevent(key_event) {
+                match key {
+                    DecodedKey::Unicode(character) => print!("{}",character),
+                    DecodedKey::RawKey(key) => print!("{:?}",key),
+                }
+            }
+        }
+
     unsafe {
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
